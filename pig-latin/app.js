efcf2411df41ce5816a4de18330951f1a2c39441
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-20 08:53:38 +0300

    wrong update made,amending

--- pig-latin/app.js
+++ pig-latin/app.js
@@ -1,1 +1,23 @@
-console.log('hey there friend!');
+function translatePigLatin(str) {
+    function doesTheWordBeginWithVowel(char){
+        let regex = /a|e|u|i|o/i;
+        let result = regex.test(char);
+        return result;
+    }
+    if(doesTheWordBeginWithVowel(str.split('')[0])){
+        return str + 'way'
+    }else{
+        let regexForAnyVowel = /a|e|u|i|o/i;
+        if(!regexForAnyVowel.test(str)){
+            return str + 'ay'
+        }else{
+            //here the word has some vowels but does not start with one and can have some in the middle
+            let regexForConsonants = /[^aeuio]+/i;
+            let initialGroup = str.match(regexForConsonants);
+            let match = str.replace(regexForConsonants, '');
+            return match + initialGroup + 'ay';
+        }
+    }
+}
+  
+console.log(translatePigLatin("rhythm"));
