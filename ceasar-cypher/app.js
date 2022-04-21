3d1ef58471dd3ef20d37d3406073e5e77ad27bcd
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-21 09:27:14 +0300

    issue solved

--- ceasar-cypher/app.js
+++ ceasar-cypher/app.js
@@ -1,1 +1,17 @@
-console.log('hey');
+function rot13(str) {
+    const alphabetLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let decodedStr = '';
+    for(let i = 0;i<str.length;i++){
+        console.log(alphabetLetters.indexOf(str[i]));
+        let index = alphabetLetters.indexOf(str[i]);
+        if(index == -1){
+            decodedStr += str[i];
+        }else{
+            let decodedCharIndex = (index + 13) % 26;
+            decodedStr += alphabetLetters[decodedCharIndex];
+        }
+    }
+    return decodedStr;
+}
+  
+console.log(rot13("SERR PBQR PNZC"));
